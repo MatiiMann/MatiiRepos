cb5b02c57b9219db4419b6ec6890a419eecc35fd
MatiiMann <mateusz.mialkas@wp.pl>
2020-02-11 16:39:20 +0100

    Added next lane to show new changes in repos

--- l_repos/src/l_repos/main.java
+++ l_repos/src/l_repos/main.java
@@ -3,5 +3,6 @@ package l_repos;
 public class main {
 	public static void main(String args[]) {
 		System.out.println("try first");
+		System.out.println("I added new code. Let's go see changes");
 	}
 }
