504edc8799a0d646a83f792d6013bf18a1fcfc5b
MatiiMann <mateusz.mialkas@wp.pl>
2020-02-12 23:39:33 +0100

    New project created. Tittle has been changed. Added new button. Added new pane with new size.

---
+++ for_git/src/main/MainWindowController.java
@@ -0,0 +1,19 @@
+package main;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainWindowController implements Initializable {
+
+    @FXML
+    Button b1;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
