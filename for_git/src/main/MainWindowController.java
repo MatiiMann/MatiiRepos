2dc630048134ebf0df80ae8c8a6e593ca8c29faf
MatiiMann <mateusz.mialkas@wp.pl>
2020-02-13 09:12:00 +0100

    Added new function of button. Button has changed color.

--- for_git/src/main/MainWindowController.java
+++ for_git/src/main/MainWindowController.java
@@ -3,6 +3,7 @@ package main;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -11,9 +12,16 @@ public class MainWindowController implements Initializable {
 
     @FXML
     Button b1;
+    @FXML
+    Label txt1;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        txt1.setText("This is a txt");
     }
+
+    public void b1_used(){
+        txt1.setText("button_has_been_pressed");
+    }
+
 }
