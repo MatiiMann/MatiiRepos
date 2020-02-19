c3b0a6c9195e98b2dbdce5158700740a1b49bc2e
MatiiMann <mateusz.mialkas@wp.pl>
2020-02-19 23:07:38 +0100

    Added reading passw from passw field

--- my_app/src/sample/Controller.java
+++ my_app/src/sample/Controller.java
@@ -2,17 +2,20 @@ package sample;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 
 import java.awt.*;
 
 public class Controller {
 
-    @FXML
-    Label txt1;
+    @FXML Label txt1;
+    @FXML PasswordField pssw;
 
     public void ButtonSignIn(){
         System.out.println("Button is working");
         txt1.setText("Logging in successful");
+        System.out.println(pssw.getText());
+
     }
 
 }
