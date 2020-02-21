7ee45fdf49d22deaec291bca3ab608e9f10d8508
MatiiMann <mateusz.mialkas@wp.pl>
2020-02-21 19:13:40 +0100

    login system works register still in progress added logo of the system

--- matii_system/src/sample/Controller.java
+++ matii_system/src/sample/Controller.java
@@ -6,9 +6,12 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
+
+    private int pos;
 
     @FXML
     public TextField username_log;
@@ -20,23 +23,36 @@ public class Controller implements Initializable {
 
     }
 
-    public void button_sign_in(){
-            System.out.println(username_log.getText());
-            System.out.println(password_log.getText());
-            System.out.println("is working");
-    }
-
     @FXML TextField username_reg;
     @FXML PasswordField password_reg;
     @FXML PasswordField password2_reg;
 
     load logins_loader = new load();
 
+    public void button_sign_in(){
+        logins_loader.login_load_write();   //load list of usernames
+        logins_loader.password_load_write();    //load list of passwords
+
+        String username = username_log.getText();   //give login from username field
+        String password = password_log.getText();   //give psw form psw field
+        int size = logins_loader.arr_long(); // give info how long is array
+        //int pos;
+
+        for(int i=0; i<size; i++){
+            if(logins_loader.give_log(i).equals(username)){
+                System.out.println("true " + username + " "  + i);
+                pos =i;
+            }
+            break;
+        }
+        if (logins_loader.give_psw(pos).equals(password)) {
+            System.out.println("password is correct");
+        }
+
+    }
+
     public void button_sign_up(){
-        System.out.println(username_reg.getText());
-        System.out.println(password_reg.getText());
-        System.out.println(password2_reg.getText());
-        logins_loader.login_load_write();
+
     }
 
 }
--- matii_system/src/sample/load.java
+++ matii_system/src/sample/load.java
@@ -1,18 +1,74 @@
 package sample;
 
+import javafx.scene.chart.ScatterChart;
+
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 
 import static java.lang.System.out;
 
-public class load {
-    File log = new File("log.txt");
-    boolean exist = log.exists();
+public class load  {
+
+        public ArrayList log_list = new ArrayList();
+        public String login;
+        public int position = 0;
+
+        public  ArrayList psw_list = new ArrayList();
+        public String psw_load;
+        public int position2 =0;
+
+    public void password_load_write(){
+        try{
+            File psw = new File("psw.txt");
+            Scanner p = new Scanner(psw);
+
+            while(p.hasNextLine()){
+                psw_load = p.nextLine();
+                psw_list.add(psw_load);
+               // System.out.println(psw_load + " " + position2);
+                position2 = position2 + 1;
+            }
+        }catch(FileNotFoundException e){
+            System.out.println(e);
+        }
+    }
 
     public void login_load_write(){
-        out.println("exist ? :" + exist);
+
+        try{
+            File log = new File("log.txt");
+            boolean exist = log.exists();
+            Scanner l = new Scanner(log);
+
+            while (l.hasNextLine()){
+                login = l.nextLine();
+                log_list.add(login);
+               // System.out.println(login+ " " + position);
+                position=position+1;
+
+            }
+
+        }catch(FileNotFoundException e){
+
+            System.out.println("File doesn't exist");
+
+        }
     }
+
+    public int arr_long(){
+        return log_list.size();
+    }
+
+    public String give_log(int x){
+        return log_list.get(x).toString();
+    }
+
+    public String give_psw(int x){
+        return psw_list.get(x).toString();
+    }
+
 }
 
